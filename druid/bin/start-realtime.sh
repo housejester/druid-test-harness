56081cdcba7387226a7acc052f86570d5f168684
James Estes <james.estes@gmail.com>
2013-02-09 22:53:39 -0700

    Upped to Druid 0.2.7.  Fixed issues with aws/s3 settings.

--- druid/bin/start-realtime.sh
+++ druid/bin/start-realtime.sh
@@ -15,10 +15,10 @@ RT_SPEC="`dirname $0`/../appevents_realtime_generated.spec"
 cat $CONFIG/base.properties $CONFIG/${NODE_TYPE}.properties |
   sed s/druid\.realtime\.specFile\=.*/druid.realtime.specFile=appevents_realtime_generated.spec/g |
   sed s/druid\.zk\.service\.host\=.*/druid.zk.service.host=$ZK_CONNECT_STRING/g |
-  sed s/com\.metamx\.aws\.accessKey\=.*/com.metamx.aws.accessKey=\"$AWS_KEY\"/g |
-  sed s/com\.metamx\.aws\.secretKey\=.*/com.metamx.aws.secretKey=\"$AWS_SECRET\"/g |
-  sed s/druid\.pusher\.s3\.bucket\=.*/druid.pusher.s3.bucket=\"$S3_BUCKET\"/g  |
-  sed s/druid\.pusher\.s3\.baseKey\=.*/druid.pusher.s3.baseKey=\"$S3_BASENAME\"/g |
+  sed s/com\.metamx\.aws\.accessKey\=.*/com.metamx.aws.accessKey=$AWS_KEY/g |
+  sed s/com\.metamx\.aws\.secretKey\=.*/com.metamx.aws.secretKey=$AWS_SECRET/g |
+  sed s/druid\.pusher\.s3\.bucket\=.*/druid.pusher.s3.bucket=$S3_BUCKET/g  |
+  sed s/druid\.pusher\.s3\.baseKey\=.*/druid.pusher.s3.baseKey=$S3_BASENAME/g |
   sed s/druid\.host\=.*/druid.host=$DRUID_HOST/g |
   sed s/druid\.port\=.*/druid.port=$DRUID_PORT/g > $RT_CONFIG
 
